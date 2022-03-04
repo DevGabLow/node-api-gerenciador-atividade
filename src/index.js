35d5cacc2a17a1df0d4306fae679376d9d07eed9
= <=>
2022-03-04 17:16:43 -0300

    all origins

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: '*' }))
 
 
 app.use(passport.initialize())
--- src/routers/auth/index.js
+++ src/routers/auth/index.js
@@ -38,6 +38,8 @@ router.get('/login', async (req, res) => {
 
                     const serialized = serialize("cookieAuth", token, {
                         httpOnly: true,
+                        secure: true,
+                        sameSite: "strict",
                         maxAge: 1 * 60 * 60 * 1000, //1h
                         path: "/"
                     });
